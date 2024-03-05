43e31440fd33cf1ab67f383af6c18896c59d8910
nerdymomocat <nerdymomocat@gmail.com>
2024-03-05 13:28:44 -0800

    Update constants and styles for tweet embeds

--- src/constants.ts
+++ src/constants.ts
@@ -73,3 +73,5 @@ export const GISCUS = key_value_from_json["giscus"] || null;
 export const THEME = key_value_from_json["theme"] || {};
 
 export const GOOGLE_SEARCH_CONSOLE_META_TAG = key_value_from_json["google-search-console-html-tag"] || null;
+
+export const FULL_WIDTH_TWEETS = key_value_from_json["full-width-tweet-embeds"] || false;
