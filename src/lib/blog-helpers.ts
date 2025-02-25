cf804d80570bc14a4b1e1a3f5137058bc610f047
nerdymomocat <nerdymomocat@gmail.com>
2025-02-24 16:52:28 -0800

    testing base path issues in integration

--- src/lib/blog-helpers.ts
+++ src/lib/blog-helpers.ts
@@ -324,7 +324,8 @@ export const buildURLToHTMLMap = async (urls: URL[]): Promise<{ [key: string]: s
 
 export const getNavLink = (nav: string) => {
   if (nav=="/rss-styles.xsl")
-    {console.log("Base path is: ", BASE_PATH);}
+    {console.log("rss-styles.xsl Base path is: ", BASE_PATH);}
+  console.log("Normal Base path is: ", BASE_PATH);
 	if (!nav && BASE_PATH) {
 		return path.join(BASE_PATH, "") + "/";
 	}
